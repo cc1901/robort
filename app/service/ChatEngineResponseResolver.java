f101fc74a12106f2a614ae91447dee76a2ef6ab8
Che Kai <chekai1985@gmail.com>
2012-12-10 22:11:48 +1100

    include <chatStateContainer> in context

--- app/service/ChatEngineResponseResolver.java
+++ app/service/ChatEngineResponseResolver.java
@@ -7,10 +7,11 @@ class ChatEngineResponseResolver {
     public static final String CONTEXT_CONTAINER_END = "</ChatStateContainer>";
 
     public ChatEngineAnswer getChatEngineResponse(String chatEngineAnswer) {
+        if (chatEngineAnswer.contains(CONTEXT_CONTAINER)) {
+            return new ChatEngineAnswer(chatEngineAnswer, "");
+        }
         int indexOfContext = chatEngineAnswer.indexOf(CONTEXT_CONTAINER);
-        int indexOfContextEnd = chatEngineAnswer.indexOf(CONTEXT_CONTAINER_END);
-        int startOfNewContext = indexOfContext + CONTEXT_CONTAINER.length();
-        String newContext = chatEngineAnswer.substring(startOfNewContext, indexOfContextEnd);
+        String newContext = chatEngineAnswer.substring(indexOfContext);
         String answer = chatEngineAnswer.substring(0, indexOfContext);
         return new ChatEngineAnswer(answer, newContext);
     }
